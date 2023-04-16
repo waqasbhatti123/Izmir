ab2403eab01ac1e05a79aa0dffb1e4612086c221
sheikh12h2 <115074118+sheikh12h2@users.noreply.github.com>
2023-04-17 02:04:36 +0500

    thk

---
+++ Shared/IZAdjustmentData.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FOS.Shared
+{
+    public class IZAdjustmentData
+    {
+        public int ID { get; set; }
+        public string ReferenceNo { get; set; }
+        public string Amount { get; set; }
+        public string BillingMonth { get; set; }
+        public string Comments { get; set; }
+        public string Adjtype { get; set; }
+    }
+}
